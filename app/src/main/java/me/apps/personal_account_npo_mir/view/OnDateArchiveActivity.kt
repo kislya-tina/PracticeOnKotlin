37331b7564dbe5a4eca66b29723845d4858475b6
victoriia sleptsova <vika.sleptsova.02@mail.ru>
2023-07-18 12:52:58 +0600

    btn back

--- app/src/main/java/me/apps/personal_account_npo_mir/view/OnDateArchiveActivity.kt
+++ app/src/main/java/me/apps/personal_account_npo_mir/view/OnDateArchiveActivity.kt
@@ -3,21 +3,35 @@ package me.apps.personal_account_npo_mir.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.View.OnClickListener
 import android.widget.Button
 import me.apps.personalaccountnpomir.R
 
-class OnDateArchiveActivity : AppCompatActivity() {
+class OnDateArchiveActivity : AppCompatActivity(), OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_on_date_archive)
 
         backButton = findViewById(R.id.back_button)
+        backButton.setOnClickListener(this)
+
     }
+
+    override fun onClick(view: View?) {
+        if(view == backButton){
+            supportFragmentManager.beginTransaction().apply {
+                addToBackStack(null)
+            }
+        }
+    }
+
 
 
     private lateinit var backButton: Button
 
 
 }
+
+
 
 
--- app/src/main/java/me/apps/personal_account_npo_mir/view/main/activities/DiagnosticActivity.kt
+++ app/src/main/java/me/apps/personal_account_npo_mir/view/main/activities/DiagnosticActivity.kt
@@ -2,11 +2,29 @@ package me.apps.personal_account_npo_mir.view.main.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
 import me.apps.personalaccountnpomir.R
 
-class DiagnosticActivity : AppCompatActivity() {
+class DiagnosticActivity : AppCompatActivity(), OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_diagnostic)
+
+        backButton = findViewById(R.id.back_button)
+        backButton.setOnClickListener(this)
     }
+
+    override fun onClick(view: View?) {
+        if(view == backButton){
+            supportFragmentManager.beginTransaction().apply {
+                addToBackStack(null)
+            }
+        }
+    }
+
+
+
+    private lateinit var backButton: Button
 }

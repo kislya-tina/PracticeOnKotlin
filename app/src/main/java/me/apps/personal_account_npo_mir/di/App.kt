325b1294580b5e8c120b38b6df75368afc4d7024
Dyusekeev Ramazan <dyusekkev.ramazan@bk.ru>
2023-08-02 12:45:28 +0600

    commit

--- app/src/main/java/me/apps/personal_account_npo_mir/di/App.kt
+++ app/src/main/java/me/apps/personal_account_npo_mir/di/App.kt
@@ -3,32 +3,36 @@ package me.apps.personal_account_npo_mir.di
 import android.app.Application
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import me.apps.personal_account_npo_mir.model.abstractions.archive_date.IArchiveDateService
+import me.apps.personal_account_npo_mir.model.abstractions.ITokenService
+import me.apps.personal_account_npo_mir.model.services.SignInService
+import me.apps.personal_account_npo_mir.model.services.MetersService
 import me.apps.personal_account_npo_mir.model.abstractions.login.ILoginService
-import me.apps.personal_account_npo_mir.model.abstractions.measures.IMeasureService
+import me.apps.personal_account_npo_mir.model.abstractions.measures.ImeasureService
 import me.apps.personal_account_npo_mir.model.abstractions.meters.IMetersService
 import me.apps.personal_account_npo_mir.model.abstractions.user_data.IUserDataService
-import me.apps.personal_account_npo_mir.model.services.*
+import me.apps.personal_account_npo_mir.model.server_connect.TokenService
+import me.apps.personal_account_npo_mir.model.services.MeasuresService
+import me.apps.personal_account_npo_mir.model.services.UserDataService
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        networkScope = CoroutineScope(Dispatchers.IO)
-        loginService = SignInService(networkScope)
-        measuresService = MeasuresService(networkScope)
-        metersService = MetersService(networkScope)
-        archiveDateService = ArchiveDateService()
         userDataService = UserDataService()
-
+        networkScope = CoroutineScope(Dispatchers.IO)
+        measuresService = MeasuresService(networkScope)
+        loginService = SignInService(networkScope)
+        metersService = MetersService(networkScope)
+         tokenService = TokenService(applicationContext)
     }
+
     // TODO: если токен есть в файл, пропускать первую активити
 
     companion object {
-        lateinit var networkScope:CoroutineScope
+        lateinit var tokenService: ITokenService
+        lateinit var measuresService: ImeasureService
         lateinit var loginService: ILoginService
-        lateinit var measuresService:IMeasureService
         lateinit var metersService: IMetersService
-        lateinit var archiveDateService: IArchiveDateService
         lateinit var userDataService: IUserDataService
+        lateinit var networkScope: CoroutineScope
     }
 }

dd547a9bfd7570d1d1c353788e661477c9605b70
Kiselev Daniil <kiselev.d.e@bk.ru>
2023-08-08 14:44:59 +0600

    id int

--- app/src/main/java/me/apps/personal_account_npo_mir/model/abstractions/meters/Meter.kt
+++ app/src/main/java/me/apps/personal_account_npo_mir/model/abstractions/meters/Meter.kt
@@ -1,6 +1,7 @@
 package me.apps.personal_account_npo_mir.model.abstractions.meters
 
 data class Meter(
+    // TODO: id must be int
     val id: String, val name: String, val serialNumber: String,
     val contractNumber: String, val address: String
 ) {
